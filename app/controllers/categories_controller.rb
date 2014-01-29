dc1765f5feb44288dca96a6fcd458dadda6a1eba
Eric Perez <ericricardoperez@gmail.com>
2014-01-28 17:36:59 -0800

    update and destroy categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,7 +14,31 @@ class CategoriesController < ApplicationController
 	def create
 		category = params[:category].permit(:name)
 		Category.create(category)
-		
+
 		redirect_to "/categories"
 	end
+
+	def edit
+		id = params[:id]
+		@category = Category.find(id)
+	end
+
+	def update
+		id = params[:id]
+		category = Category.find(id)
+		category.name = params[:category][:name]
+		category.save
+
+		redirect_to category_path(category)
+	end
+
+	def destroy
+		id = params[:id]
+		category = Category.find(id)
+		category.destroy
+
+		redirect_to "/categories"
+	end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ RailStore::Application.routes.draw do
     get "/categories", to: "categories#index"
     get "/categories/new", to: "categories#new"
     get "/categories/:id", to: "categories#show", as: :category
+    get "/categories/:id/edit", to: "categories#edit"
+    patch "/categories/:id", to: "categories#update"
+    delete "/categories/:id", to: "categories#destroy"
     post "/categories", to: "categories#create"
 end
